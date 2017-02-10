edfcae57ea23774e2e3f5c2dee2fb05e1efbcc09
Jorge Álvarez <Jorge lvarez>
2017-02-10 08:32:21 -0600

    Se ha hecho un cambio

--- POP6Jahaziel/src/pop6jahaziel/POP6Jahaziel.java
+++ POP6Jahaziel/src/pop6jahaziel/POP6Jahaziel.java
@@ -6,27 +6,14 @@ public class POP6Jahaziel {
 
     public static void main(String[] args) {
         
-        
     }
 
     public String listar(ArrayList Arreglo){
         String lista="";
         for (int i = 0; i < Arreglo.size(); i++) {
-            
+            lista+=i+". "+Arreglo.get(i)+"\n";
         }
         return lista;
-        
-    
-        
-    }
-  
-    public static String puto(String p){
-        while(true){
-            p = "puto";
-            System.out.println(puto(p));
-        }
-        
     }
     
-
 }
