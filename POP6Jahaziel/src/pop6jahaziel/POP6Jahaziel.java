345f9dc80a207c0aaf4be987bee6ade468af0e2e
Jorge Álvarez <Jorge lvarez>
2017-02-09 20:14:08 -0600

    Cambio de prueba de Jorge

--- POP6Jahaziel/src/pop6jahaziel/POP6Jahaziel.java
+++ POP6Jahaziel/src/pop6jahaziel/POP6Jahaziel.java
@@ -1,14 +1,21 @@
 package pop6jahaziel;
 
+import java.util.ArrayList;
+
 public class POP6Jahaziel {
 
-  
     public static void main(String[] args) {
         
         
-        
-        
-        
+    }
+
+    public String listar(ArrayList Arreglo){
+        String lista="";
+        for (int i = 0; i < Arreglo.size(); i++) {
+            
+        }
+        return lista;
     }
     
+
 }
